93d3188513fc51a1fb0ee13648c97a2f22a98992
Surendra <sky958447@gmail.com>
2025-01-30 11:14:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function validEmail(str) {
   //your JS code here.
+	let isEmail =/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(str);
+	 return isEmail;
 }
 
 // Do not change the code below.
